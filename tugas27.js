9551b00ebb774d978bd15ca7bfd05f060e754a65
Decky Fajar Sidiq <46802206+deckyfajars@users.noreply.github.com>
2021-07-01 17:01:20 +0700

    Add files via upload

---
+++ tugas27.js
@@ -0,0 +1,9 @@
+function ubah(){
+  var kalimat = ["No", "rumah saya", 30, "A"];
+  var kalimat2 =["alan pangeran Tirtayasa No", 70];
+  hasil = String(kalimat);
+  hasil2 = String(kalimat2);
+  console.log(hasil, hasil2);
+}
+
+ubah();
